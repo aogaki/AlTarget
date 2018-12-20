1627419f87ffa3710814b37af82865b69f566662
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2018-12-20 16:15:53 +0200

    Change script for this simulation

--- script.sh
+++ script.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-nEvents=1000000
+nEvents=300000000
 
-for T in 200 250 300 350 400 450 500 550 600
+for T in 70 7
 do
   for ((ene=1;ene<=100;ene++))
   do
     echo "/InSb/Geo/T $T" > tmp.mac
-    echo "/InSb/Primary/K $ene" >> tmp.mac
     echo "/run/beamOn $nEvents" >> tmp.mac
 
     ./example tmp.mac
-    hadd -f "resultT$T""Ene$ene".root result_t*
+    hadd -f "resultT$T".root result_t*
   done
 done
