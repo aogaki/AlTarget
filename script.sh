01509236a53a6bba4366b76cbfafd23198f3f8c0
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2018-12-20 16:17:19 +0200

    ahhhhhhh.  Script!

--- script.sh
+++ script.sh
@@ -4,20 +4,9 @@ nEvents=300000000
 
 for T in 70 7
 do
-<<<<<<< HEAD
   echo "/InSb/Geo/T $T" > tmp.mac
   echo "/run/beamOn $nEvents" >> tmp.mac
 
   ./example tmp.mac
   hadd -f "resultT$T".root result_t*
-=======
-  for ((ene=1;ene<=100;ene++))
-  do
-    echo "/InSb/Geo/T $T" > tmp.mac
-    echo "/run/beamOn $nEvents" >> tmp.mac
-
-    ./example tmp.mac
-    hadd -f "resultT$T".root result_t*
-  done
->>>>>>> 1627419f87ffa3710814b37af82865b69f566662
 done
