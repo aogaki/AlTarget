3152979d7cb387a539b82cbe903cfc34a7eeaaef
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2019-01-14 13:55:32 +0200

    test

--- Analysis/test_cpp.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./test_cpp.so: /opt/ROOT/include/RVersion.h /opt/ROOT/include/RConfig.h /opt/ROOT/include/TClass.h /opt/ROOT/include/TDictAttributeMap.h /opt/ROOT/include/TInterpreter.h /opt/ROOT/include/TROOT.h /opt/ROOT/include/TBuffer.h /opt/ROOT/include/TMemberInspector.h /opt/ROOT/include/TError.h /opt/ROOT/include/RtypesImp.h /opt/ROOT/include/TIsAProxy.h /opt/ROOT/include/TFileMergeInfo.h /opt/ROOT/include/TCollectionProxyInfo.h /opt/ROOT/bin/rootcling
-test_cpp__ROOTBUILDVERSION= 6.17/01
